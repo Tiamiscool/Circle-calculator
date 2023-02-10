8a16f1943c56ea2949d404de1479f9a08eb4ce12
Tiamiscool <107582387+Tiamiscool@users.noreply.github.com>
2023-02-09 21:24:18 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ var r = window.prompt("Type in the radius:");
 
 document.write("<p>The area is about " + pi * r * r + "</p>");
 document.write("<p>The circumference is about " + r * 2 * pi + "</p>");
-document.write("<p>The diameter is about " + 2 * pi + "</p>");
+document.write("<p>The diameter is about " + 2 * r + "</p>");
